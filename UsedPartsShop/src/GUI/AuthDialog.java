1201eb4237603a2c8c1a546f26b8d3deb5cdb522
DmitrySkv <rainbowworlds@mail.ru>
2013-06-19 01:52:01 +0400

    modyfied

--- UsedPartsShop/src/GUI/AuthDialog.java
+++ UsedPartsShop/src/GUI/AuthDialog.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package GUI.Authorisation;
+package GUI;
 
 import BusinessLogic.AuthoriseInfo;
 import BusinessLogic.Customer;
--- UsedPartsShop/src/GUI/NewBid.java
+++ UsedPartsShop/src/GUI/NewBid.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package GUI.Authorisation;
+package GUI;
 
 import BusinessLogic.Bid;
 import BusinessLogic.Seller;
